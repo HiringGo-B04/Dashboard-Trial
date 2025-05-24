dd9db555f6195adcee5400a6fa64e96b308af063
DaoistXuandu <akbarraihan2005@gmail.com>
2025-05-25 05:31:07 +0700

    minor change on lecturer

---
+++ src/app/dashboard/lecturer/controller.ts
@@ -0,0 +1,54 @@
+
+'use client'
+
+import { backend_link } from "@/utils";
+
+// admin dashboard
+const user_route = "/api/account/admin/user"
+
+async function adminGetAllUsers(token: string) {
+    const response = await fetch(`${backend_link}${user_route}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+    }).then(response => response.json());
+
+    return response
+}
+
+async function adminDeleteUser(token: string, username: string) {
+    const response = await fetch(`${backend_link}${user_route}`, {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify({
+            email: username,
+        })
+    }).then(response => response.json());
+
+    return response;
+}
+
+async function adminUpdateUser(token: string, requestBody: any) {
+    const response = await fetch(`${backend_link}${user_route}`, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        },
+        body: JSON.stringify(requestBody)
+    }).then(response => response.json());
+
+    return response;
+}
+
+
+export {
+    adminGetAllUsers,
+    adminDeleteUser,
+    adminUpdateUser
+}
--- src/app/dashboard/lecturer/page.tsx
+++ src/app/dashboard/lecturer/page.tsx
@@ -1,5 +1,35 @@
+"use client"
+
+import { logout } from "@/app/auth/controller";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
 export default function DashboardDosen() {
+    const router = useRouter()
+
+    const [totalOfCourse, setTotalOfCourse] = useState(0);
+    const [totalOfAssitant, setTotalOfAssistant] = useState(0);
+    const [totalOfEnrolVacancy, setTotalOfVacancy] = useState(0);
+
+    useEffect(() => {
+        
+
+    }, [router])
+
     return (
-        <div>Dosen</div>
+        <div>
+            <div className="flex flex-col gap-10 p-10">
+                <div className="flex flex-row gap-10">
+                    <button onClick={e => logout(router)}>Logout</button>
+                    <a href="/dashboard/admin/register-admin">Create Admin</a>
+                    <a href="/dashboard/admin/register-lecturer">Create Lecturer</a>
+                </div>
+                <div className="flex flex-row gap-10">
+                    <div>Total of Course: {totalOfCourse}</div>
+                    <div>Number of Teaching Assitant: {totalOfAssitant}</div>
+                    <div>Number of Remaining Vacancy: {totalOfEnrolVacancy}</div>
+                </div>
+            </div>
+        </div>
     )
 }
